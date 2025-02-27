c70756036f6766cd74af2876c47ff7d3fc942e58
Alexander2049 <a.alexander.2049@gmail.com>
2025-02-27 13:06:38 +0200

    Separated dev server from AssettoCorsaSDK class

---
+++ src/dev.ts
@@ -0,0 +1,11 @@
+import AssettoCorsaSDK from ".";
+
+const acsdk = new AssettoCorsaSDK({
+  broadcastName: "",
+  broadcastPassword: "",
+  updateIntervalMs: 500
+});
+
+acsdk.addListener("physics", (physics) => {
+  console.log(physics.speedKmh, physics.rpm, physics.gear);
+});
